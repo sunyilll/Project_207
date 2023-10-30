c42045ab60da3ec639e1d49741457ccb3464c9e6
OliviaY1 <yanqianyanxi@outlook.com>
2023-10-30 14:38:33 -0400

    Basic Information of ViewModel and SendMessageViewModel added

--- src/main/java/interface_adapter/send_message/SendMessageViewModel.java
+++ src/main/java/interface_adapter/send_message/SendMessageViewModel.java
@@ -1,4 +1,19 @@
 package main.java.interface_adapter.send_message;
 
-public class SendMessageViewModel {
+import main.java.interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeListener;
+
+public class SendMessageViewModel extends ViewModel {
+    private SendMessageState state = new SendMessageState();
+    public SendMessageViewModel(){super("send message");}
+    public void setState(SendMessageState state){this.state = state;}
+    public SendMessageState getState(){return state;}
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    // Presenter call firePropertyChanged() to let ViewModel know state changed
+    public void firePropertyChanged(){support.firePropertyChange("state", null, this.state);}
+    public void addPropertyChangeListener(PropertyChangeListener listener){
+        support.addPropertyChangeListener(listener);
+    }
 }
