7c17f140a694a62deca563a8bb780dcde0b2c19a
OliviaY1 <yanqianyanxi@outlook.com>
2023-11-19 15:39:24 -0500

    ViewWithHomeBar & FrameModel is created and done.
    a Model for View with Home Bar is created. A Model Frame is created so that every view can follow a same frame.

--- src/main/java/view/FrameModel.java
+++ src/main/java/view/FrameModel.java
@@ -1,17 +1,12 @@
 package main.java.view;
-
 import javax.swing.*;
 
 public class FrameModel extends javax.swing.JFrame{
     String viewName;
-    JPanel viewPanel;
-    FrameModel(){
+    protected FrameModel(String viewName){
         this.setName(viewName);
-        this.setSize(720, 1280);
-        this.setContentPane(viewPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setVisible(true);
+        this.setSize(480, 800);
     }
 }
 
