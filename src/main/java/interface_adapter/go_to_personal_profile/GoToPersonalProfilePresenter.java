52ea83f06e8045154e89ad6d25d587fc0df67e4b
JoyceQu <joycequ04@gmail.com>
2023-11-20 01:32:19 -0500

    Updated interface adapter for go to personal profile use case

--- src/main/java/interface_adapter/go_to_personal_profile/GoToPersonalProfilePresenter.java
+++ src/main/java/interface_adapter/go_to_personal_profile/GoToPersonalProfilePresenter.java
@@ -1,5 +1,15 @@
 package main.java.interface_adapter.go_to_personal_profile;
-import main.java.use_case.go_to_personal_profile.GoToProfileOutputBoundary;
+import main.java.use_case.go_to_personal_profile.GoToPersonalProfileOutputBoundary;
+import main.java.use_case.go_to_personal_profile.GoToPersonalProfileOutputData;
 
-public class GoToPersonalProfilePresenter implements GoToProfileOutputBoundary {
+public class GoToPersonalProfilePresenter implements GoToPersonalProfileOutputBoundary {
+    @Override
+    public void prepareSuccessView(GoToPersonalProfileOutputData goToPersonalProfileOutputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+
+    }
 }
--- src/main/java/interface_adapter/go_to_personal_profile/GoToPersonalProfileViewModel.java
+++ src/main/java/interface_adapter/go_to_personal_profile/GoToPersonalProfileViewModel.java
@@ -5,6 +5,26 @@ import main.java.interface_adapter.ViewModel;
 import java.beans.PropertyChangeListener;
 
 public class GoToPersonalProfileViewModel extends ViewModel {
+    public final String TITLE_LABEL = "My Profile View";
+    public final String NICKNAME_TEXT = "Nickname";
+    public final String ABOUT_ME_LABEL = "About Me";
+    public final String ABOUT_ME_TEXT = "HELLO WORLD";
+    public final String PERSONALITY_TAGS_LABEL = "Personality Tags";
+    public final String COURSES_TO_TEACH_LABEL = "Courses to Teach";
+    public final String TUTOR_RATING_LABEL = "Tutor Rating";
+    public final String COURSES_TO_LEARN_LABEL = "Courses to Learn";
+    public final String STUDENT_RATING_LABEL = "Student Rating";
+    public final String EDIT_PROFILE_BUTTON_LABEL = "Edit Profile";
+    public final String SIGN_OUT_BUTTON_LABEL = "Sign Out";
+    public final String HOME_BUTTON_LABEL = "Home";
+    public final String CHAT_LIST_BUTTON_LABEL = "Chat";
+    public final String PRIVATE_PROFILE_BUTTON_LABEL = "My Profile";
+
+
+    public GoToPersonalProfileViewModel() {
+        super("personal profile");
+    }
+
     public GoToPersonalProfileViewModel(String viewName) {
         super(viewName);
     }
