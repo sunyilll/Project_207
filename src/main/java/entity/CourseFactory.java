df8251af1d02c0c95f9f683cc861f0d2a090d5f0
OliviaY1 <yanqianyanxi@outlook.com>
2023-11-26 16:35:30 -0500

    Used for FileCourseDataAccessObject.java. create() not implemented yet.

---
+++ src/main/java/entity/CourseFactory.java
@@ -0,0 +1,14 @@
+package main.java.entity;
+
+import main.java.data_access.FileCourseDataAccessObject;
+
+public class CourseFactory {
+    private final FileUserDataAccessObject fileUserDataAccessObject;
+    public CourseFactory(FileUserDataAccessObject fileUserDataAccessObject){
+        this.fileUserDataAccessObject = fileUserDataAccessObject;
+    }
+    public Course create(String courseCode, String[] tutorids, String[] studentids){
+        Course c = new Course(courseCode, "CourseNameNotImplemented"); //todo: courseName not implemented
+
+    }
+}
