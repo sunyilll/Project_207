793e81c7096b6817a3de47e319aa7af8d3088edb
AmandaWu226 <amandawu109@gmail.com>
2023-11-28 12:07:40 -0500

    Updated LoginPresenter to switch view to search view if login button is clicked.

--- src/main/java/interface_adapter/login/LoginPresenter.java
+++ src/main/java/interface_adapter/login/LoginPresenter.java
@@ -2,6 +2,8 @@ package main.java.interface_adapter.login;
 
 import main.java.interface_adapter.To_signup.ToSignupController;
 import main.java.interface_adapter.ViewManagerModel;
+import main.java.interface_adapter.search.SearchState;
+import main.java.interface_adapter.search.SearchViewModel;
 import main.java.interface_adapter.signup.SignupState;
 import main.java.interface_adapter.signup.SignupViewModel;
 import main.java.interface_adapter.To_signup.ToSignupViewModel;
@@ -12,26 +14,32 @@ import main.java.use_case.login.LoginOutputData;
 public class LoginPresenter implements LoginOutputBoundary {
 
     private final LoginViewModel loginViewModel;
+    private final SearchViewModel searchViewModel;
+
     private final ToSignupViewModel toSignupViewModel;
     private ViewManagerModel viewManagerModel;
 
     public LoginPresenter(ViewManagerModel viewManagerModel,
                           ToSignupViewModel toSignupViewModel,
-                          LoginViewModel loginViewModel) {
+                          LoginViewModel loginViewModel,
+                          SearchViewModel searchViewModel) {
         this.viewManagerModel = viewManagerModel;
+        this.searchViewModel = searchViewModel;
         this.toSignupViewModel = toSignupViewModel;
         this.loginViewModel = loginViewModel;
     }
 
     @Override
     public void prepareSuccessView(LoginOutputData response) {
-        // On success, switch to the signup view.
-//        SignupState signupState = SignupViewModel.getState();
-//        signupState.setUsername(response.getUsername());
-//        this.signupViewModel.setState(signupState);
-//        this.signupViewModel.firePropertyChanged();
-//        this.viewManagerModel.setActiveView(signupViewModel.getViewName());
-//        this.viewManagerModel.firePropertyChanged();
+        // On success, switch to the search view.
+        SearchState searchState = searchViewModel.getState();
+        searchState.setUsername(response.getUserID());
+        searchViewModel.setState(searchState);
+
+        searchViewModel.firePropertyChanged();
+        viewManagerModel.setActiveView(searchViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+        System.out.println(1112);
     }
 
     @Override
