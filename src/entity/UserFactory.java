c4ac6d7be789379401ea1a458eaa424f8af1bae8
JoyceQu <joycequ04@gmail.com>
2023-12-02 21:03:27 -0500

    Overloaded create method with more inputs

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -1,5 +1,8 @@
 package entity;
 
+import java.util.List;
+import java.util.Map;
+
 public class UserFactory {
     /**
      * Requires: password is valid.
@@ -11,5 +14,35 @@ public class UserFactory {
 
     public User create(String userid, String nickname, String password) {
         return new User(userid, nickname, password);
+    }
+
+    public User create(String userid, String nickname, String password, String pronouns, String description, String tutorAvailability, List<String> personalityTag, List<String> coursesToTeach, List<String> coursesToLearn, List<String> preferredModeOfLearning, List<String> preferredModeOfTeaching, Map<String, Integer> expectedWage, Map<String, Integer> expectedPrice) {
+
+        User user = new User(userid, nickname, password);
+        user.setDescription(description);
+        user.setPronouns(pronouns);
+        user.setTutorAvailability(tutorAvailability);
+        for (String tag : personalityTag) {
+            user.addPersonalityTag(tag);
+        }
+        for (String course : coursesToLearn) {
+            user.addCourseToTeach(course);
+        }
+        for (String course : coursesToTeach) {
+            user.addCourseToTeach(course);
+        }
+        for (String mode : preferredModeOfLearning) {
+            user.addPreferredModeOfLearning(mode);
+        }
+        for (String mode : preferredModeOfTeaching) {
+            user.addPreferredModeOfTeaching(mode);
+        }
+        for (String course : expectedWage.keySet()) {
+            user.setExpectedWage(course, expectedWage.get(course));
+        }
+        for (String course : expectedPrice.keySet()) {
+            user.setExpectedPrice(course, expectedPrice.get(course));
+        }
+        return user;
     }
 }
