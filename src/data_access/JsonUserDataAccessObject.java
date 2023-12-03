158cd82e682ee50b0bd013c8b6ad1ab2fcd418c0
JoyceQu <joycequ04@gmail.com>
2023-12-02 22:20:52 -0500

    implemented get user function

--- src/data_access/JsonUserDataAccessObject.java
+++ src/data_access/JsonUserDataAccessObject.java
@@ -2,6 +2,7 @@ package data_access;
 
 import entity.User;
 
+import entity.UserBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -32,15 +33,117 @@ public class JsonUserDataAccessObject implements SignupUserDataAccessInterface, 
 
     public static void main(String[] args) {
         JsonUserDataAccessObject jsonUserDataAccessObject = new JsonUserDataAccessObject("data/users.json");
-        // User testUser = new User("test", "test", "test");
-        // jsonUserDataAccessObject.save(testUser);
+//         User testUser = new User("test", "test", "test");
+//         jsonUserDataAccessObject.save(testUser);
+        // System.out.println(jsonUserDataAccessObject.toString());
         System.out.println(jsonUserDataAccessObject.existsByName("test"));
+        System.out.println(jsonUserDataAccessObject.get("test").getPersonalityTags());
     }
 
     // TODO: Refactor code so that we are searching for the user by ID instead of nickname
     @Override
     public User get(String userID) {
-        return null;
+        if (!existsByName(userID)) {
+            return null;
+        } else {
+            UserBuilder userBuilder = new UserBuilder();
+            JSONObject userJSON = userFile.getJSONObject(userID);
+            String nickname = userJSON.getString("nickname");
+            String password = userJSON.getString("password");
+            userBuilder.create(userID, nickname, password);
+            try {
+                String pronouns = userJSON.getString("pronouns");
+                userBuilder.setPronouns(pronouns);
+                }
+            catch (Exception e) {
+                // do nothing
+            }
+            try {
+                String description = userJSON.getString("description");
+                userBuilder.setDescription(description);
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                String tutorAvailability = userJSON.getString("tutor_availability");
+                userBuilder.setTutorAvailability(tutorAvailability);
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray personalityTags = userJSON.getJSONArray("personality_tags");
+                for (int i = 0; i < personalityTags.length(); i++) {
+                    userBuilder.addPersonalityTag(personalityTags.getString(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray coursesToLearn = userJSON.getJSONArray("courses_to_learn");
+                for (int i = 0; i < coursesToLearn.length(); i++) {
+                    userBuilder.addCoursesToLearn(coursesToLearn.getString(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray coursesToTeach = userJSON.getJSONArray("courses_to_teach");
+                for (int i = 0; i < coursesToTeach.length(); i++) {
+                    userBuilder.addCoursesToTeach(coursesToTeach.getString(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray modeOfLearning = userJSON.getJSONArray("mode_of_learning");
+                for (int i = 0; i < modeOfLearning.length(); i++) {
+                    userBuilder.addPreferredModeOfLearning(modeOfLearning.getString(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray modeOfTeaching = userJSON.getJSONArray("mode_of_teaching");
+                for (int i = 0; i < modeOfTeaching.length(); i++) {
+                    userBuilder.addPreferredModeOfTeaching(modeOfTeaching.getString(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray studentRatings = userJSON.getJSONArray("student_ratings");
+                for (int i = 0; i < studentRatings.length(); i++) {
+                    userBuilder.addStudentRating(studentRatings.getInt(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONArray tutorRatings = userJSON.getJSONArray("tutor_ratings");
+                for (int i = 0; i < tutorRatings.length(); i++) {
+                    userBuilder.addTutorRating(tutorRatings.getInt(i));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONObject expectedWage = userJSON.getJSONObject("expected_wage");
+                for (String course : expectedWage.keySet()) {
+                    userBuilder.setExpectedWage(course, expectedWage.getInt(course));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            try {
+                JSONObject expectedPrice = userJSON.getJSONObject("expected_price");
+                for (String course : expectedPrice.keySet()) {
+                    userBuilder.setExpectedPrice(course, expectedPrice.getInt(course));
+                }
+            } catch (Exception e) {
+                // do nothing
+            }
+            return userBuilder.getUser();
+        }
     }
 
     @Override
