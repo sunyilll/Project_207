7a99877420c72cc148592cd32826445eb64e5ed9
Alysa Liu <dengban.mew@gmail.com>
2023-12-04 02:35:50 -0500

    added feature to gotochannel

--- src/data_access/JsonUserDataAccessObject.java
+++ src/data_access/JsonUserDataAccessObject.java
@@ -267,6 +267,22 @@ public class JsonUserDataAccessObject implements SignupUserDataAccessInterface,
         }
 
     }
+    public String getChatChannelsWith2Users(String user1, String user2) throws RuntimeException {
+        SendBirdAPI sendBirdAPIObject = new SendBirdAPI("https://api-1F4C3D4F-01DB-4A99-8704-BE4CB1FE3AE5.sendbird.com/v3",
+                "1F4C3D4F-01DB-4A99-8704-BE4CB1FE3AE5",
+                "0ecfef313ab2989479b70e30e3ee37a1d105b770");
+        try {
+            ArrayList<ChatChannel> channels = sendBirdAPIObject.getAllChatChannels(user1+"%C2"+user2);
+            if (channels == null || channels.isEmpty()) {
+                String channelUrl = sendBirdAPIObject.createChatChannel(user1, user2);
+                return channelUrl;
+            }
+            return channels.get(0).getChannelURL();
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
     @Override
     public Channel getChannel(String currentUserid, String targetUserid) {
