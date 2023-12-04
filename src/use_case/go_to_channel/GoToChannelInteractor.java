82cc2708de6e08a73813cf862ec62fda3e67801c
Alysa Liu <dengban.mew@gmail.com>
2023-12-04 02:20:38 -0500

    some changes with goto channel use case

--- src/use_case/go_to_channel/GoToChannelInteractor.java
+++ src/use_case/go_to_channel/GoToChannelInteractor.java
@@ -1,10 +1,11 @@
 package use_case.go_to_channel;
 
+import data_access.JsonUserDataAccessObject;
 import entity.ChatChannel;
 import entity.User;
 
 public class GoToChannelInteractor implements GoToChannelInputBoundary{
-//    final GoToChannelDataAccessInterface goToChannelDataAccessObject;
+    final JsonUserDataAccessObject goToChannelDataAccessObject;
     final GoToChannelOutputBoundary goToChannelPresenter;
     public GoToChannelInteractor(GoToChannelOutputBoundary goToChannelPresenter) {
 //        this.goToChannelDataAccessObject = goToChannelDataAccessObject;
@@ -15,6 +16,13 @@ public class GoToChannelInteractor implements GoToChannelInputBoundary{
     public void execute(GoToChannelInputData goToChannelInputData) {
         User currentUser = goToChannelInputData.getCurrentUser();
         ChatChannel currentChannel = goToChannelInputData.getCurrentChannel();
+        if (currentChannel.getChannelURL() != "DNE"){
+            GoToChannelOutputData goToChannelOutputData = new GoToChannelOutputData(currentUser, currentChannel);
+        } else{
+            goToChannelDataAccessObject.
+
+        }
+
         GoToChannelOutputData goToChannelOutputData = new GoToChannelOutputData(currentUser, currentChannel);
         goToChannelPresenter.prepareSuccessView(goToChannelOutputData);
 
