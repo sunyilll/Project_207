f76d518f65e40a96949b6d93d59b65ba79e74baa
OliviaY1 <yanqianyanxi@outlook.com>
2023-12-04 12:28:56 -0500

    UPDATE TEST FILE

--- src/view/ChannelView/ChannelView.java
+++ src/view/ChannelView/ChannelView.java
@@ -176,6 +176,7 @@ public class ChannelView extends JPanel implements ActionListener, PropertyChang
                                     currentState.getUser_id(),
                                     currentState.getChannel()
                             );
+                            messageInputField.setText("");
                         }
                     }
                 }
