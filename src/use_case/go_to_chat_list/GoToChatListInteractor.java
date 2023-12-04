a7f48206034ee22019396b453d0ea2f7737e7aeb
OliviaY1 <yanqianyanxi@outlook.com>
2023-12-04 02:35:15 -0500

    Does not rise prepareFailView(). If no channels relate to current user, GoToChatListInteractor put an empty arraylist to chat list.

--- src/use_case/go_to_chat_list/GoToChatListInteractor.java
+++ src/use_case/go_to_chat_list/GoToChatListInteractor.java
@@ -3,6 +3,7 @@ package use_case.go_to_chat_list;
 import entity.ChatChannel;
 import entity.User;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class GoToChatListInteractor implements GoToChatListInputBoundary {
@@ -14,14 +15,15 @@ public class GoToChatListInteractor implements GoToChatListInputBoundary {
     }
     @Override
     public void execute() {
-        try {
+        try{
             ArrayList<ChatChannel> chatChannels = goToChatPageDataAccessObject.getAllChatChannels();
             GoToChatListOutputData goToChatListOutputData = new GoToChatListOutputData(chatChannels, goToChatPageDataAccessObject.getCurrentUser());
             goToChatListPresenter.prepareSuccessView(goToChatListOutputData);
-            System.out.println("I am at step 3");
-        } catch (RuntimeException e) {
-            goToChatListPresenter.prepareFailView(e.toString());
-            System.out.println("I am at step 4");
+            return;
+        } catch (RuntimeException e){
+            ArrayList<ChatChannel> chatChannels = new ArrayList<>();
+            GoToChatListOutputData goToChatListOutputData = new GoToChatListOutputData(chatChannels, goToChatPageDataAccessObject.getCurrentUser());
+            goToChatListPresenter.prepareSuccessView(goToChatListOutputData);
         }
     }
 
